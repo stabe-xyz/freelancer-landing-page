5e09d2feeb8077ad49c05f7cf276a126a4c048fb
Héctor Valls <hvalls@protonmail.com>
2024-06-19 18:34:52 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return <footer className='p-10 flex justify-center space-x-20'>
-    <div>© 2024 {{ name }}</div>
-    <div><a className='hover:text-violet-500 transition' href="{{ linkedin_profile_url }}" target="_blank">LinkedIn</a></div>
+    <div>© 2024 {{ stabe.name }}</div>
+    <div><a className='hover:text-violet-500 transition' href="{{ stabe.linkedin_profile_url }}" target="_blank">LinkedIn</a></div>
   </footer>
 }
 
